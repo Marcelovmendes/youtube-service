c35da62f489ece1a742c99aa46c10e3108f8f4f3
Marcelovmendes <marcelovmendescontato@gmail.com>
2026-01-11 22:23:03 -0300

    feat: add CORS config and improve session serialization

--- src/main/java/com/example/youtube/common/config/SessionConfig.java
+++ src/main/java/com/example/youtube/common/config/SessionConfig.java
@@ -2,6 +2,8 @@ package com.example.youtube.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
@@ -14,10 +16,13 @@ public class SessionConfig {
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName("YOUTUBE_SESSION");
-        serializer.setUseHttpOnlyCookie(true);
-        serializer.setSameSite("None");
-        serializer.setUseSecureCookie(false);
         serializer.setCookiePath("/");
+        serializer.setUseHttpOnlyCookie(true);
         return serializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
     }
 }
